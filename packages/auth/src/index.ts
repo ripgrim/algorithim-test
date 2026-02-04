0d7c9889f235105648530ff8682a362574609616
grim <75869731+ripgrim@users.noreply.github.com>
2026-02-03 19:07:11 -0800

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,19 @@
+import { db } from "@algorithim-test/db";
+import * as schema from "@algorithim-test/db/schema/auth";
+import { env } from "@algorithim-test/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { nextCookies } from "better-auth/next-js";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [nextCookies()],
+});
